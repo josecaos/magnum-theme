82f36d639db0d292de3d19a9891640ba33f6e487
josecaos <jazzvoon@gmail.com>
2019-03-11 11:00:36 -0600

    Fix a problema con idioma en productos

--- broadcast.php
+++ broadcast.php
@@ -29,29 +29,59 @@ if (get_locale() == 'es_MX' ):
         <div class="broadcast-container row">
           <div class="broadcast-titles col-3">
             <ul>
-              <li><a href="<?php echo get_category_link(2);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-LiveU.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(17);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-lawo.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(3);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-aws-elemental.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(4);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-yamaha.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(5);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-decimator.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(6);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-shure.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(20);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-abonair.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(124);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-point.source.audio.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(126);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-cedar.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(19);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-sennheiser.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(128);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-brainstorm.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(130);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-vimeo.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(11);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-telos.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(15);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-omnia.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(230);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-infinity.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(12);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-linear-acoustic.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(234);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-minnetonka-audio.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(14);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-25-seven-systems.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(7);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-kramer.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(238);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-on-stage.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(10);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-video-devices.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(9);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-sound-devices.png" alt=""></a></li>
-              <li><a href="<?php echo get_category_link(242);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-jutel.png" alt=""></a></li>
+              <?php if (get_locale() == 'es_MX'): ?>
+
+                <li><a href="<?php echo get_category_link(2);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-LiveU.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(17);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-lawo.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(3);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-aws-elemental.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(4);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-yamaha.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(5);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-decimator.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(6);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-shure.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(20);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-abonair.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(124);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-point.source.audio.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(126);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-cedar.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(19);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-sennheiser.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(128);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-brainstorm.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(130);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-vimeo.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(11);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-telos.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(15);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-omnia.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(230);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-infinity.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(12);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-linear-acoustic.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(234);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-minnetonka-audio.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(14);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-25-seven-systems.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(7);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-kramer.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(238);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-on-stage.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(10);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-video-devices.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(9);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-sound-devices.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(242);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-jutel.png" alt=""></a></li>
+
+              <?php elseif (get_locale() == 'en_US'):?>
+
+                <li><a href="<?php echo get_category_link(77);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-LiveU.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(73);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-lawo.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(65);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-aws-elemental.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(87);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-yamaha.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(69);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-decimator.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(85);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-shure.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(63);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-abonair.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(159);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-point.source.audio.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(157);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-cedar.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(83);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-sennheiser.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(155);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-brainstorm.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(161);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-vimeo.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(96);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-telos.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(79);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-omnia.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(232);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-infinity.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(75);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-linear-acoustic.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(236);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-minnetonka-audio.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(58);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-25-seven-systems.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(71);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-kramer.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(240);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-on-stage.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(90);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-video-devices.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(98);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-sound-devices.png" alt=""></a></li>
+                <li><a href="<?php echo get_category_link(244);?>"> <img src="<?php echo get_template_directory_uri();?>/images/sections/broadcast/brands/magnum-broadcast-jutel.png" alt=""></a></li>
+
+              <?php endif; ?>
             </ul>
           </div>
           <div id="broadcast-cats" class="broadcast-elements col-9">
